78a87b9b8ad336c78df9a575acf607363a053d1c
wallebach <114591465+wallebach@users.noreply.github.com>
2023-12-13 17:09:47 +0100

    Lesson 8: Refactoring your tests

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -7,9 +7,10 @@ import {console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 contract DeployFundMe is Script {
-    function run() external {
+    function run() external returns (FundMe) {
         vm.startBroadcast();
-        new FundMe();
+        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         vm.stopBroadcast();
+        return fundMe;
     }
 }
