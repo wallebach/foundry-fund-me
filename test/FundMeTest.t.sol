d5d350fa217cadde1a85ba4923bfae1f9b6767ed
wallebach <114591465+wallebach@users.noreply.github.com>
2023-12-13 16:30:22 +0100

    Lesson 7: Running tests on chains forks

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -18,4 +18,8 @@ contract FundMeTest is Test {
     function testOwnerIsMsgSender() public {
         assertEq(fundMe.i_owner(), address(this));
     }
+
+    function testPriceFeedVersionIsAccurate() public {
+        assertEq(fundMe.getVersion(), 4);
+    }
 }
