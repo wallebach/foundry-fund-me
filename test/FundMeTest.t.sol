05db5539feba1567464f1d46e6edcb7cef97dee7
wallebach <114591465+wallebach@users.noreply.github.com>
2023-12-13 15:43:27 +0100

    Lesson 5: Debug your Solidity tests

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -14,4 +14,8 @@ contract FundMeTest is Test {
     function testMinimumDollarIsFive() public {
         assertEq(fundMe.MINIMUM_USD(), 5e18);
     }
+
+    function testOwnerIsMsgSender() public {
+        assertEq(fundMe.i_owner(), address(this));
+    }
 }
